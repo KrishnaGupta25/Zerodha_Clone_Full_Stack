40ebf1852c674e409d61369686b6952fb9e1a2b6
KrishnaGupta25 <krishnagupta44382@gmail.com>
2025-08-02 13:59:38 +0530

    Update Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("https://zerodha-clone-full-stack-backend.onrender.com/newOrder/allHoldings").then((res) => {
+    axios.get("https://zerodha-clone-full-stack-backend.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
